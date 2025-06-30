b65998ac61c54f9775bbcf384f2f5d91064c5949
AfraBinteAzad <afrabinteazad@gmail.com>
2025-07-01 02:29:48 +0600

    Update after saving

--- coffee.py
+++ coffee.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 @app.route('/order', methods=['POST'])
 def order():
@@ -12,5 +12,13 @@ def order():
     coffee = data.get('coffee')
     return jsonify({'message': f'Thank you for ordering {coffee}!'})
 
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
